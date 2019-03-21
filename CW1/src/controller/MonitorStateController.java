a05f8670c2a38e559cfb65d8f2b4a418c318dcdc
mcguile <cmcguile@gmail.com>
2019-03-21 16:37:39 +0000

    Comments added.

--- CW1/src/controller/MonitorStateController.java
+++ CW1/src/controller/MonitorStateController.java
@@ -13,10 +13,21 @@ import model.Server;
 import model.ServerList;
 
 
+/**
+ * MVC Controller for controlling the addition and removal of servers,
+ * altering the processing speed, and adding observers to servers for
+ * the MonitorStateGUI to observe.
+ *
+ */
 public class MonitorStateController {
 	private MonitorStateGUI view;
 	private ServerList serverList;
 	
+	/**
+	 * Constructor of the controller
+	 * @param view The GUI displaying server processes and the queue
+	 * @param serverList the list of servers to be controlled
+	 */
 	public MonitorStateController(MonitorStateGUI view, ServerList serverList){
 		this.view = view;
 		this.serverList = serverList;
@@ -27,6 +38,11 @@ public class MonitorStateController {
 	}
 	
 	
+	/**
+	 * Begin gradually adding customers from the new orders file
+	 * by responding to start simulation button press. 
+	 * A thread is created to handle this.
+	 */
 	public class StartSimualationListner implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 				Qthread q = new Qthread();
@@ -37,6 +53,11 @@ public class MonitorStateController {
 		}
 	}
 	
+	/**
+	 * Add a server to the server list and allow the view to
+	 * observe the server.
+	 * Limit the number of servers to 4.
+	 */
 	public class AddServerListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			int numOfServers = serverList.getServers().size();
@@ -50,16 +71,19 @@ public class MonitorStateController {
 		}
 	}
 	
-	
-	
-	
+	/**
+	 * Remove a server from the server list.
+	 */
 	public class RemoveServerListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			serverList.removeServer();
 		}
 	}
 	
-	// inner class SpeedListener responds when user sets the Speed via the slider
+	/**
+	 * SpeedListener responds to user control of the slider on the GUI.
+	 * Controls the speed of a single item being processed by a server.
+	 */
 	public class SpeedListener implements ChangeListener {
 		@Override
 		public void stateChanged(ChangeEvent e) {
@@ -67,7 +91,5 @@ public class MonitorStateController {
 			Server.setThreadSleepTime(speed);
 			view.sliderLable.setText("Speed per item: " + Integer.toString(Server.getThreadSleepTime()/1000)+ " seconds"); 
 		}
-		
 	}
-
 }
