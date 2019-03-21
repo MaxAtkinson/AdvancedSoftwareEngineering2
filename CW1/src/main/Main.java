ec026d4ffebfdeeca41cecaaa5c0b1b213a62f89
gordo851 <gr2@hw.ac.uk>
2019-03-21 12:19:50 +0000

    Java doc on Main and Custom Exceptions

--- CW1/src/main/Main.java
+++ CW1/src/main/Main.java
@@ -11,9 +11,23 @@ import model.ServerList;
 import view.AddOrdersView;
 import view.MonitorStateGUI;
 
+/**
+ * Main method for running the programme. Sets up the MVC classes of ServerList, MonitorStateGui,
+ * and MonitorStateController.
+ *
+ */
 public class Main {
 	//Main method instantiating single instance of FileManagerIO and running GUI
-
+/**
+ * Main instantiates the singleton fileManager and reads in the product CSV to create the programmes menu.
+ * Finally all previous orders are read in so that the final report will contain all orders and not just 
+ * that days.
+ * 
+ * 
+ * @param args
+ * @throws InvalidProductPriceException 
+ * @throws InvalidProductIdentifierException
+ */
 	public static void main(String[] args) throws InvalidProductPriceException, InvalidProductIdentifierException {
 		//MVC Set-up
 		ServerList serversModel = new ServerList();
